224321ffab61fb81a8da53d13770374bfcc0573d
barnabedikartola <barnabedikartola@gmail.com>
2026-02-07 15:16:48 -0300

    new example scripts

--- usr/share/biglinux/biglinux-settings/example/example.sh
+++ usr/share/biglinux/biglinux-settings/example/example.sh
@@ -1,87 +1,108 @@
 #!/bin/bash
 
 # check current status
-check_state() {
+if [ "$1" == "check" ]; then
+  # for KDE Plasma only
   if [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"Plasma"* ]];then
     if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
       echo "true"
     else
       echo "false"
     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
-#     if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
-#       echo "true"
-#     else
-#       echo "false"
-#     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]];then
-#     if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
-#       echo "true"
-#     else
-#       echo "false"
-#     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
-#     if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
-#       echo "true"
-#     else
-#       echo "false"
-#     fi
+  # for GNOME ONly
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
+    if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
+      echo "true"
+    else
+      echo "false"
+    fi
+  # for XFCE only
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]];then
+    if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
+      echo "true"
+    else
+      echo "false"
+    fi
+  # for CINNAMON only
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
+    if [[ "$(LANG=C some Command)" == "true" ]];then #or if some Command &>/dev/null;then # if command response exit 0
+      echo "true"
+    else
+      echo "false"
+    fi
   fi
-}
 
 # change the state
-toggle_state() {
-  new_state="$1"
+elif [ "$1" == "toggle" ]; then
+  state="$2"
+  # for KDE Plasma only
   if [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"Plasma"* ]];then
-    if [[ "$new_state" == "true" ]];then
-      some command as user
-      pkexec some command as root
+    # if the state is true
+    if [ "$state" == "true" ]; then
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
       exitCode=$?
+    # if the state is false
     else
-      some command as user
-      pkexec some command as root
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "uninstall" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
       exitCode=$?
     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
-#     if [[ "$new_state" == "true" ]];then
-#       some command
-#       exitCode=$?
-#     else
-#       some command
-#       exitCode=$?
-#     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]];then
-#     if [[ "$new_state" == "true" ]];then
-#       some command
-#       exitCode=$?
-#     else
-#       some command
-#       exitCode=$?
-#     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
-#     if [[ "$new_state" == "true" ]];then
-#       some command
-#       exitCode=$?
-#     else
-#       some command
-#       exitCode=$?
-#     fi
+  # for GNOME ONly
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
+    # if the state is true
+    if [ "$state" == "true" ]; then
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+      exitCode=$?
+    # if the state is false
+    else
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "uninstall" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+      exitCode=$?
+    fi
+  # for XFCE only
+  elif [ -n "$(grep SHMC  $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml)" ];then
+    # if the state is true
+    if [ "$state" == "true" ]; then
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+      exitCode=$?
+    # if the state is false
+    else
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "uninstall" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+      exitCode=$?
+    fi
+  # for CINNAMON only
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
+    # if the state is true
+    if [ "$state" == "true" ]; then
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+      exitCode=$?
+    # if the state is false
+    else
+      # execute a command as a user
+      any command as user
+      # execute a command as root, prompting for a password only once.
+      pkexec $PWD/example/exampleRun.sh "uninstall" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+      exitCode=$?
+    fi
   fi
   exit $exitCode
-}
-
-# Executes the function based on the parameter
-case "$1" in
-    "check")
-        check_state
-        ;;
-    "toggle")
-        toggle_state "$2"
-        ;;
-    *)
-        echo "Use: $0 {check|toggle} [true|false]"
-        echo "  check          - Check current status"
-        echo "  toggle <state> - Changes to the specified state"
-        exit 1
-        ;;
-esac
+fi
