78b37e05ec5cc1d690e0eb7af9910427767926b8
barnabedikartola <barnabedikartola@gmail.com>
2026-02-07 22:45:26 -0300

    fix exit code

--- usr/share/biglinux/biglinux-settings/usability/recentFiles.sh
+++ usr/share/biglinux/biglinux-settings/usability/recentFiles.sh
@@ -45,10 +45,10 @@ elif [ "$1" == "toggle" ]; then
   if [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"Plasma"* ]];then
     if [ "$state" == "true" ]; then
       $PWD/usability/recentFilesRun.sh "enable" #"$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-      exit=$?
+      exitCode=$?
     else
       $PWD/usability/recentFilesRun.sh "disable" #"$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-      return $?
+      exitCode=$?
     fi
 #   elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
 #     if [ "$state" == "true" ]; then
