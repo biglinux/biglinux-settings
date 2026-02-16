659995f924bacf8a0e657a731d77f6f8b2c92baa
barnabedikartola <barnabedikartola@gmail.com>
2026-02-16 16:29:22 -0300

    update run comfyUI AI

--- usr/share/biglinux/biglinux-settings/ai/comfyUIRun.sh
+++ usr/share/biglinux/biglinux-settings/ai/comfyUIRun.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-cd $HOME/ComfyUI
-bin/python main.py
+# check current status
+if [ "$1" == "check" ]; then
+  if [ -n "$(ps aux | grep -i "$HOME/ComfyUI/bin/python $HOME/ComfyUI/main.py" | grep -v grep)" ]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+
+elif [ "$1" == "toggle" ]; then
+  state="$2"
+  if [ "$state" == "true" ]; then
+    $HOME/ComfyUI/bin/python $HOME/ComfyUI/main.py --listen 0.0.0.0 &
+    exitCode=$?
+  else
+    kill $(ps aux | grep -i "$HOME/ComfyUI/bin/python $HOME/ComfyUI/main.py" | grep -v grep | awk '{print $2}')
+    exitCode=$?
+  fi
+  exit $exitCode
+fi
