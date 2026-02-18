9a638e292da3a3426b5daa49b7adf27a93bdc512
Wesley Ribeiro <wesleyjcr@gmail.com>
2026-02-17 21:43:46 -0300

    Added the functionality to activate and deactivate numlock in the cinnamon interface.

--- usr/share/biglinux/biglinux-settings/usability/numLock.sh
+++ usr/share/biglinux/biglinux-settings/usability/numLock.sh
@@ -20,12 +20,13 @@ if [ "$1" == "check" ]; then
 #     else
 #       echo "false"
 #     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
-#     if [[ "$someTest" == "true" ]];then
-#       echo "true"
-#     else
-#       echo "false"
-#     fi
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
+    recent_status=$(gsettings get org.cinnamon.desktop.peripherals.keyboard numlock-state)
+    if [[ "$recent_status" == "true" ]]; then
+      echo "true"
+    else
+      echo "false"
+    fi
   fi
 
 # change the state
@@ -42,7 +43,7 @@ elif [ "$1" == "toggle" ]; then
         return $?
     fi
 #   elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
-#     if [[ "$new_state" == "true" ]];then
+#     if [[ "$state" == "true" ]];then
 #         some command
 #         exitCode=$?
 #     else
@@ -50,21 +51,23 @@ elif [ "$1" == "toggle" ]; then
 #         exitCode=$?
 #     fi
 #   elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]];then
-#     if [[ "$new_state" == "true" ]];then
+#     if [[ "$state" == "true" ]];then
 #         some command
 #         exitCode=$?
 #     else
 #         some command
 #         exitCode=$?
 #     fi
-#   elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
-#     if [[ "$new_state" == "true" ]];then
-#         some command
-#         exitCode=$?
-#     else
-#         some command
-#         exitCode=$?
-#     fi
+  elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
+    if [[ "$state" == "true" ]];then
+        gsettings set org.cinnamon.desktop.peripherals.keyboard numlock-state true
+        numlockx on
+        exitCode=$?
+    else
+        gsettings set org.cinnamon.desktop.peripherals.keyboard numlock-state false
+        numlockx off 
+        exitCode=$?
+    fi
   fi
   exit $exitCode
 fi
