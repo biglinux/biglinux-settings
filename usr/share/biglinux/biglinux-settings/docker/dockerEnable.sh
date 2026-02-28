489648db51156f011ce1eded8d419ab33b681c30
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 18:54:36 -0300

    Update dockerEnable.sh

--- usr/share/biglinux/biglinux-settings/docker/dockerEnable.sh
+++ usr/share/biglinux/biglinux-settings/docker/dockerEnable.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # check current status
+# action=$1
 if [ "$1" == "check" ]; then
   if [[ "$(systemctl is-enabled docker)" == "enabled" ]] && [[ "$(systemctl is-active docker)" == "active" ]] && [[ "$(systemctl is-active docker.socket)" == "active" ]];then
     echo "true"
@@ -9,18 +10,17 @@ if [ "$1" == "check" ]; then
   fi
 
 # change the state
+# action=$1
+# state=$2
 elif [ "$1" == "toggle" ]; then
-  state="$2"
-  if [ "$state" == "true" ]; then
+  if [ "$2" == "true" ]; then
     if ! pacman -Q biglinux-docker-config &>/dev/null; then
       pkexec $PWD/docker/dockerEnableRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
     else
       pkexec $PWD/docker/dockerEnableRun.sh "enable" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
     fi
-    exitCode=$?
   else
     pkexec $PWD/docker/dockerEnableRun.sh "disable" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-    exitCode=$?
   fi
-  exit $exitCode
+  exit $?
 fi
