0424e9849e7184830a84d9fdc7702534047a63d1
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 19:27:50 -0300

    Update meltdownMitigations.sh

--- usr/share/biglinux/biglinux-settings/performance/meltdownMitigations.sh
+++ usr/share/biglinux/biglinux-settings/performance/meltdownMitigations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # check current status
+# action=$1
 if [ "$1" == "check" ]; then
   if [[ -n "$(grep "mitigations=off" /proc/cmdline)" ]];then
     echo "true"
@@ -9,14 +10,13 @@ if [ "$1" == "check" ]; then
   fi
 
 # change the state
+# action=$1
+# state=$2
 elif [ "$1" == "toggle" ]; then
-  state="$2"
-  if [ "$state" == "true" ]; then
+  if [ "$2" == "true" ]; then
     pkexec $PWD/performance/meltdownMitigationsRun.sh "enable" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-    exitCode=$?
   else
     pkexec $PWD/performance/meltdownMitigationsRun.sh "disable" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-    exitCode=$?
   fi
-  exit $exitCode
+  exit $?
 fi
