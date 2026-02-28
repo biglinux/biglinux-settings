686a63d506a5c666a7a50362763fd8ee3f262ecb
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 20:29:39 -0300

    Update ollamaVulkan.sh

--- usr/share/biglinux/biglinux-settings/ai/ollamaVulkan.sh
+++ usr/share/biglinux/biglinux-settings/ai/ollamaVulkan.sh
@@ -24,9 +24,11 @@ if [ "$1" == "check" ]; then
 elif [ "$1" == "toggle" ]; then
   if [ "$2" == "true" ]; then
     pkexec $PWD/ai/ollamaVulkanRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+    exitCode=$?
     info
   else
     pkexec $PWD/ai/ollamaVulkanRun.sh "uninstall" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
+    exitCode=$?
   fi
-  exit $?
+  exit $exitCode
 fi
