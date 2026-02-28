de0fd43d019112262f6402dbcb6a0ad355d24003
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 18:49:39 -0300

    Update reverse-mouse_scroll.sh

--- usr/share/biglinux/biglinux-settings/devices/reverse-mouse_scroll.sh
+++ usr/share/biglinux/biglinux-settings/devices/reverse-mouse_scroll.sh
@@ -4,6 +4,7 @@
 kcminputrcFile="$HOME/.config/kcminputrc"
 
 # check current status
+# action=$1
 if [ "$1" == "check" ]; then
   if [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"Plasma"* ]];then
     if [[ "$(LANG=C LANGUAGE=C kreadconfig6 --file "$kcminputrcFile" --group "Mouse" --key "NaturalScroll")" == "true" ]];then
@@ -32,18 +33,17 @@ if [ "$1" == "check" ]; then
   fi
 
 # change the state
+# action=$1
+# state=$2
 elif [ "$1" == "toggle" ]; then
-  state="$2"
   if [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"Plasma"* ]];then
-    if [ "$state" == "true" ]; then
+    if [ "$2" == "true" ]; then
       kwriteconfig6 --file "$kcminputrcFile" --group "Mouse" --key "NaturalScroll" "true"
 
       devices=$(qdbus6 org.kde.KWin | grep "/org/kde/KWin/InputDevice/event")
       for device in $devices; do
         qdbus6 org.kde.KWin "$device" org.kde.KWin.InputDevice.naturalScroll true &> /dev/null
       done
-
-      exitCode=$?
     else
       kwriteconfig6 --file "$kcminputrcFile" --group "Mouse" --key "NaturalScroll" "false"
 
@@ -51,8 +51,6 @@ elif [ "$1" == "toggle" ]; then
       for device in $devices; do
         qdbus6 org.kde.KWin "$device" org.kde.KWin.InputDevice.naturalScroll false &> /dev/null
       done
-
-      exitCode=$?
     fi
   # elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
   #   if [ "$state" == "true" ]; then
@@ -79,5 +77,5 @@ elif [ "$1" == "toggle" ]; then
   #       exitCode=$?
   #   fi
   fi
-  exit $exitCode
+  exit $?
 fi
