4aef9aecf52ddcd084941731510f00c461d86a1b
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 19:15:22 -0300

    Update ollamaLab.sh

--- usr/share/biglinux/biglinux-settings/ai/ollamaLab.sh
+++ usr/share/biglinux/biglinux-settings/ai/ollamaLab.sh
@@ -5,6 +5,7 @@ export TEXTDOMAINDIR="/usr/share/locale"
 export TEXTDOMAIN=biglinux-settings
 
 # check current status
+# action=$1
 if [ "$1" == "check" ]; then
   if pacman -Q ollama-lab-bin &>/dev/null; then
     echo "true"
@@ -13,14 +14,13 @@ if [ "$1" == "check" ]; then
   fi
 
 # change the state
+# action=$1
+# state=$2
 elif [ "$1" == "toggle" ]; then
-  state="$2"
-  if [ "$state" == "true" ]; then
+  if [ "$2" == "true" ]; then
     pkexec $PWD/ai/ollamaLabRun.sh "install" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-    exitCode=$?
   else
     pkexec $PWD/ai/ollamaLabRun.sh "uninstall" "$USER" "$DISPLAY" "$XAUTHORITY" "$DBUS_SESSION_BUS_ADDRESS" "$LANG" "$LANGUAGE"
-    exitCode=$?
   fi
-  exit $exitCode
+  exit $?
 fi
