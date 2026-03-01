03e0287b08da9af53fd72a77053957705a18d801
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 21:28:16 -0300

    Update numLock.sh

--- usr/share/biglinux/biglinux-settings/usability/numLock.sh
+++ usr/share/biglinux/biglinux-settings/usability/numLock.sh
@@ -38,38 +38,30 @@ elif [ "$1" == "toggle" ]; then
     if [ "$2" == "true" ]; then
         pkexec kwriteconfig6 --group "General" --key "Numlock" --file "/etc/sddm.conf" "on"
         kwriteconfig6 --group "Keyboard" --key "NumLock" --file "$HOME/.config/kcminputrc" "0"
-        exit=$?
     else
         pkexec kwriteconfig6 --group "General" --key "Numlock" --file "/etc/sddm.conf" "off"
         kwriteconfig6 --group "Keyboard" --key "NumLock" --file "$HOME/.config/kcminputrc" "1"
-        return $?
     fi
 #   elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
 #     if [[ "$2" == "true" ]];then
 #         some command
-#         exitCode=$?
 #     else
 #         some command
-#         exitCode=$?
 #     fi
 #   elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]];then
 #     if [[ "$2" == "true" ]];then
 #         some command
-#         exitCode=$?
 #     else
 #         some command
-#         exitCode=$?
 #     fi
   elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
     if [[ "$2" == "true" ]];then
         gsettings set org.cinnamon.desktop.peripherals.keyboard numlock-state true
         numlockx on
-        exitCode=$?
     else
         gsettings set org.cinnamon.desktop.peripherals.keyboard numlock-state false
         numlockx off 
-        exitCode=$?
     fi
   fi
-  exit $exitCode
+  exit $?
 fi
