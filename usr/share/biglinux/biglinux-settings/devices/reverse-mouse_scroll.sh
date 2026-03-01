9a88f9073a8c37c5f6a87b5d72e0f2480580b972
prschopf <16743256+prschopf@users.noreply.github.com>
2026-02-28 22:29:45 -0300

    Update reverse-mouse_scroll.sh

--- usr/share/biglinux/biglinux-settings/devices/reverse-mouse_scroll.sh
+++ usr/share/biglinux/biglinux-settings/devices/reverse-mouse_scroll.sh
@@ -55,26 +55,19 @@ elif [ "$1" == "toggle" ]; then
   # elif [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]];then
   #   if [ "$2" == "true" ]; then
   #       some command
-  #       exitCode=$?
   #   else
   #       some command
-  #       exitCode=$?
   #   fi
   # elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]];then
   #   if [ "$2" == "true" ]; then
-  #       some command
-  #       exitCode=$?
   #   else
   #       some command
-  #       exitCode=$?
   #   fi
   # elif [[ "$XDG_CURRENT_DESKTOP" == *"Cinnamon"* ]] || [[ "$XDG_CURRENT_DESKTOP" == *"X-Cinnamon"* ]];then
   #   if [ "$2" == "true" ]; then
   #       some command
-  #       exitCode=$?
   #   else
   #       some command
-  #       exitCode=$?
   #   fi
   fi
   exit $?
